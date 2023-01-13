8f0504b716643c9d1087dc4ee36efc11ae7e86ae
lakhdar hafsi <lacdart2@hotmail.com>
2023-01-13 02:35:28 +0100

    creates PUT request route

--- src/modules/application/routes.js
+++ src/modules/application/routes.js
@@ -3,7 +3,40 @@ import { databasePrisma } from "../../prismaClient.js";
 
 export const applicationsRouter = express.Router();
 
-// Handling request using router
-applicationsRouter.get("/", async (req, res) => {
-  res.send("This is the app request");
+
+// Put application router :
+
+
+applicationsRouter.put('/', async (req, res) => {
+  try {
+    const { applicantId, listingId, coverLetter } = req.body;
+    const newApplication = await databasePrisma.application.create({
+      data: {
+        applicantId,
+        user: {
+          connect: { id: applicantId },
+        },
+        listing: {
+          connect: { id: listingId },
+        },
+        Offers: {
+          create: [],
+        },
+        coverLetter,
+      },
+      select: {
+        id: true,
+        applicantId: true,
+        user: true,
+        listing: true,
+        Offers: true,
+        listingId: true,
+        coverLetter: true,
+      },
+    });
+    res.status(201).json({ application: newApplication });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
+
