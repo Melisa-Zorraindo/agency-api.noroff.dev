c933af90a4ecd36779fc86951539f1bc64c7a7a7
lakhdar hafsi <lacdart2@hotmail.com>
2023-01-19 18:06:13 +0100

    splits and updates code in routes.js

--- src/modules/application/routes.js
+++ src/modules/application/routes.js
@@ -1,40 +1,34 @@
 import express from "express";
-import { databasePrisma } from "../../prismaClient.js";
+import { handleEdit } from "./controllers/controllerEdit.js";
 
 export const applicationsRouter = express.Router();
 
+applicationsRouter.get("/", async (req, res) => {
+  res.send("This is the app request");
+});
 
 
-applicationsRouter.put('/:id', async (req, res) => {
+// Handling request using router
+applicationsRouter.get("/", async (req, res) => {
+  res.send("This is the app request");
+});
+
+// PUT /application/:id
+applicationsRouter.put("/:id", async (req, res) => {
   try {
-    const { applicantId, listingId, coverLetter } = req.body;
-    const newApplication = await databasePrisma.application.update({
-      data: {
-        applicantId,
-        user: {
-          connect: { id: applicantId },
-        },
-        listing: {
-          connect: { id: listingId },
-        },
-        Offers: {
-          create: [],
-        },
-        coverLetter,
-      },
-      select: {
-        id: true,
-        applicantId: true,
-        user: true,
-        listing: true,
-        Offers: true,
-        listingId: true,
-        coverLetter: true,
-      },
-    });
-    res.status(201).json({ application: newApplication });
+    const result = await handleEdit(req);
+
+    res.status(200).json(result);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    console.log(err);
+
+    const errorObject = await JSON.parse(err.message);
+    if (errorObject.status) {
+      res.status(errorObject.status).json(errorObject.message);
+    } else {
+      res.status(500).json("Internal server error.");
+    }
   }
 });
+
 
